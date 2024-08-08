2c0401d68c7f35ba1a348b36cc01945fec166ae2
KashafNaveedAhmad <kashafnaveed329@gmail.com>
2024-08-08 14:05:24 +0500

    basic structure

---
+++ tik-tak-toe.py
@@ -0,0 +1,12 @@
+def printboard():
+    pass
+
+print("WELCOME TO TIC TAC TOE!!")
+print(f" 0 | 1 | 2")
+print(f"---|---|---")
+print(f" 3 | 4 | 5")
+print(f"---|---|---")
+print(f" 6 | 7 | 8")
+
+ 
+
